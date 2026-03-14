832e68ebcf7493f2b8f27adc0bc3e895b094746c
PelleyJ <pelleysam@gmail.com>
2026-03-14 23:11:32 +0900

    Week 2 Assignment init push

--- backend/controllers/contactsController.js
+++ backend/controllers/contactsController.js
@@ -27,7 +27,73 @@ const getSingleContact = async (req, res) => {
   }
 };
 
+const createContact = async (req, res) => {
+  try {
+    const db = await connectToDatabase();
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const result = await db.collection("contacts").insertOne(contact);
+    res.status(201).json(result);
+  } catch (error) {
+    res.status(500).json({ message: "Error creating contact" });
+  }
+};
+
+const updateContact = async (req, res) => {
+  try {
+    const contactId = new mongodb.ObjectId(req.params.id);
+    const db = await connectToDatabase();
+
+    const contact = {
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      favoriteColor: req.body.favoriteColor,
+      birthday: req.body.birthday
+    };
+
+    const result = await db
+      .collection("contacts")
+      .replaceOne({ _id: contactId }, contact);
+
+    if (result.matchedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ message: "Error updating contact" });
+  }
+};
+
+const deleteContact = async (req, res) => {
+  try {
+    const contactId = new mongodb.ObjectId(req.params.id);
+    const db = await connectToDatabase();
+
+    const result = await db.collection("contacts").deleteOne({ _id: contactId });
+
+    if (result.deletedCount === 0) {
+      return res.status(404).json({ message: "Contact not found" });
+    }
+
+    res.status(200).json({ message: "Contact deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ message: "Error deleting contact" });
+  }
+};
+
 module.exports = {
   getAllContacts,
-  getSingleContact
+  getSingleContact,
+  createContact,
+  updateContact,
+  deleteContact
 };
